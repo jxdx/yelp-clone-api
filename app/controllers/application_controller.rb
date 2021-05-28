52c413368a12f7ffd5cf7984c378495de27790f1
JXDX <josh@junglescout.com>
2021-05-27 20:20:11 -0700

    implement jwt authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+  include ExceptionHandler
+
+  # called before every action on controllers
+  before_action :authorize_request
+  attr_reader :current_user
+
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
+
+  def api_error(messages, status = 400)
+    render json: {error: { messages: Array.wrap(messages) }}, status: status
+  end
+
+  private
+
+  # Check for valid request token and return user
+  def authorize_request
+    @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+  end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  skip_before_action :authorize_request, only: :create
+
+  # POST /signup
+  # return authenticated token upon signup
+  def create
+    user = User.create!(user_params)
+    auth_token = AuthenticateUser.new(user.email, user.password).call
+    json_response({auth_token: auth_token}, :created)
+  end
+
+  private
+
+  def user_params
+    params.permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
+end
